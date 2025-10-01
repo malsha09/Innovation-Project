381ceaf46d4f1f4b27cd7dca41cfacef0bc0e1d7
Bahaa <bahaaghazi11@gmail.com>
2025-10-01 18:58:09 +1000

    Update phase_3_sequence_models.py

--- data/phase_3_sequence_models.py
+++ data/phase_3_sequence_models.py
@@ -16,7 +16,7 @@ tf.random.set_seed(1337)
 
 # %%
 # ---------- CONFIG ----------
-CSV_PATH = 'all_datasets.npz.zip'  # upload this file in Colab or put in repo
+CSV_PATH = 'all_datasets_compressed.npz.zip'  # upload this file in Colab or put in repo
 TARGET = 'nat_demand'
 TIME = 'datetime'
 
